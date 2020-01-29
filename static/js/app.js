5f1b50f8989d25f7ee51cfcafce5ca7d4a3838ab
hayden4davey <hayden4davey@gmail.com>
2020-01-29 11:07:45 -0500

    add optional filters

--- static/js/app.js
+++ static/js/app.js
@@ -20,33 +20,49 @@ button.on("click", function(){
     var countryValue = countryElement.property("value");
     var shapeValue = shapeElement.property("value");
 
-    // Filter data based on inputs
-    var filteredData = tableData.filter(tableData => tableData.datetime === dateValue &&
-                                                    tableData.city === cityValue.toLowerCase() &&
-                                                    tableData.state === stateValue.toLowerCase() &&
-                                                    tableData.country === countryValue.toLowerCase() &&
-                                                    tableData.shape === shapeValue.toLowerCase()
-        );
-  
-    console.log(filteredData);
+    // Filter data based on optional inputs
+    if (dateValue != ""){
+        var filteredData1 = tableData.filter(tableData => tableData.datetime === dateValue)
+    } else{
+        var filteredData1 = tableData;
+    }
+    if (cityValue != ""){
+        var filteredData2 = filteredData1.filter(filteredData1 => filteredData1.city === cityValue.toLowerCase())
+    } else{
+        var filteredData2 = filteredData1;
+    }
+    if (stateValue != ""){
+        var filteredData3 = filteredData2.filter(filteredData2 => filteredData2.state === stateValue.toLowerCase())
+    } else{
+        var filteredData3 = filteredData2;
+    }
+    if (countryValue != ""){
+        var filteredData4 = filteredData3.filter(filteredData3 => filteredData3.country === countryValue.toLowerCase())
+    } else{
+        var filteredData4 = filteredData3;
+    }
+    if (shapeValue != ""){
+        var filteredData5 = filteredData4.filter(filteredData4 => filteredData4.shape === shapeValue.toLowerCase())
+    } else{
+        var filteredData5 = filteredData4;
+    }
+        
+    console.log(filteredData5);
 
     var table = '<tbody>'
 
     // Loop through filtered table and add it to html table
-    for(i = 0;i < filteredData.length; i++){
+    for(i = 0;i < filteredData5.length; i++){
         table+= '<tr>';
-        table+= '<td>' + filteredData[i].datetime + '</td>';
-        table+= '<td>' + filteredData[i].city + '</td>';
-        table+= '<td>' + filteredData[i].state + '</td>';
-        table+= '<td>' + filteredData[i].country + '</td>';
-        table+= '<td>' + filteredData[i].shape + '</td>';
-        table+= '<td>' + filteredData[i].duration + '</td>';
-        table+= '<td>' + filteredData[i].comments + '</td>';
+        table+= '<td>' + filteredData5[i].datetime + '</td>';
+        table+= '<td>' + filteredData5[i].city + '</td>';
+        table+= '<td>' + filteredData5[i].state + '</td>';
+        table+= '<td>' + filteredData5[i].country + '</td>';
+        table+= '<td>' + filteredData5[i].shape + '</td>';
+        table+= '<td>' + filteredData5[i].duration + '</td>';
+        table+= '<td>' + filteredData5[i].comments + '</td>';
         table+= '</tr>';
-    }
-    table+='</tbody>';
-    document.getElementById('table-body').innerHTML = table;
-
-
-
+        }
+        table+='</tbody>';
+        document.getElementById('table-body').innerHTML = table;
 })
